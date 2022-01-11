b3213402ffa8cfde76b2970aa124420ccf661742
Kirzakhov <59237098+Kirzakhov@users.noreply.github.com>
2022-01-11 12:31:28 +0530

    display details

---
+++ HelloWorld.java
@@ -0,0 +1,24 @@
+package mypack;
+import java.util.*;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your name");
+		String name = sc.nextLine();
+		System.out.println("Enter your age");
+		int age = sc.nextInt();
+		System.out.println("Enter your qualification");
+		String qualification = sc.next();
+		System.out.println("Enter your fees");
+		float fees = sc.nextFloat();
+		
+		System.out.println("My name is " + name);
+		System.out.println("My age is " + age);
+		System.out.println("My qualification is " + qualification);
+		System.out.println("My fees is " + fees);
+		sc.close();
+	}
+
+}
