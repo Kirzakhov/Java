7ea7f91e7cc82fcb1194e891e193070a00241593
Kirzakhov <59237098+Kirzakhov@users.noreply.github.com>
2022-01-12 20:55:16 +0530

    Add files via upload

---
+++ Pos_neg.java
@@ -0,0 +1,12 @@
+package mypack;
+import java.io.*;
+
+public class Pos_neg {
+	public static void main(String args[]) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter a number");
+		int n = Integer.parseInt(br.readLine());
+		String result = (n>0)?"positive":"negative";
+		System.out.println("The given number is " + result);
+	}
+}
