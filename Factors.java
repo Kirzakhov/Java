825590ffd4c9444385b6bf7b6da67343ad0bad6a
Kirzakhov <59237098+Kirzakhov@users.noreply.github.com>
2022-01-12 20:54:59 +0530

    Add files via upload

---
+++ Factors.java
@@ -0,0 +1,21 @@
+package mypack;
+import java.util.*;
+
+public class Factors {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int n = sc.nextInt();
+		int sum = 0;
+		for(int i = 1; i <= n; i++) 
+			if(n % i == 0) {
+				System.out.println("The factors of " +n+ " are " +i);
+				sum += i;
+			}
+		System.out.println("The sum of all the factors is " +sum);
+		sc.close();
+	}
+
+}
