c86864c79bf3896dc9315e5ccf71cb62cf06c6ac
Kirzakhov <59237098+Kirzakhov@users.noreply.github.com>
2022-01-19 18:08:58 +0530

    Count the total number of vowels in a sentence

---
+++ FindVowel.java
@@ -0,0 +1,18 @@
+package myString;
+
+public class FindVowel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "Hello how are you";
+		s.toLowerCase();
+		int c = 0;
+		for(int i = 0; i < s.length(); i++) {
+			char ch = s.charAt(i);
+			if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch=='u')
+				c++;
+		}
+		System.out.println("Total number of vowels: " +c);
+	}
+
+}
